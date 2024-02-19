2737877749e23b538eaac3ec260f7487fe8bc84c
Luca Benetti <lucabbenetti@hotmail.com>
2024-02-19 10:15:00 -0300

    tests: add unit tests for book service

---
+++ src/BookLibrary.Domain.Tests/Services/BookServiceTests.cs
@@ -0,0 +1,33 @@
+﻿using BookLibrary.Domain.Contracts;
+using BookLibrary.Domain.Entities;
+using BookLibrary.Domain.Enums;
+using BookLibrary.Domain.Services;
+using Moq;
+using System.Linq.Expressions;
+using Xunit;
+
+namespace BookLibrary.Domain.Tests.Services
+{
+    public class BookServiceTests
+    {
+        private readonly BookService _bookService;
+        private readonly Mock<IBookRepository> _bookRepository;
+
+        public BookServiceTests()
+        {
+            _bookRepository = new Mock<IBookRepository>();
+            _bookService = new BookService(_bookRepository.Object);
+
+            _bookRepository.Setup(x => x.FilterAsync(It.IsAny<Expression<Func<Book, bool>>>()))
+                .Returns<Expression<Func<Book, bool>>>(predicate => Task.FromResult((IReadOnlyList<Book>)BookServiceTestsFixture.GetBooks().Where(predicate).ToList()));
+        }
+
+        [Fact]
+        public async Task BookService_FilterByAuthor()
+        {
+            var books = await _bookService.GetAsync(SearchType.Author, "coel");
+            
+            Assert.True(books.All(x => x.Id == BookServiceTestsFixture.GetTheAlchemistBook().Id));
+        }
+    }
+}
---
+++ src/BookLibrary.Domain.Tests/Services/BookServiceTestsFixture.cs
@@ -0,0 +1,48 @@
+﻿using BookLibrary.Domain.Entities;
+
+namespace BookLibrary.Domain.Tests.Services
+{
+    public class BookServiceTestsFixture
+    {
+        public static IQueryable<Book> GetBooks()
+        {
+            return new EnumerableQuery<Book>(new List<Book>()
+            {
+                GetPrideAndPrejudiceBook(),
+                GetTheAlchemistBook()
+            });
+        }
+
+        public static Book GetPrideAndPrejudiceBook()
+        {
+            return new Book()
+            {
+                Id = 1,
+                Title = "Pride and Prejudice",
+                FirstName = "Jane",
+                LastName = "Austen",
+                TotalCopies = 100,
+                CopiesInUse = 80,
+                Type = "Hardcover",
+                ISBN = "1234567891",
+                Category = "Fiction"
+            };
+        }
+
+        public static Book GetTheAlchemistBook()
+        {
+            return new Book()
+            {
+                Id = 5,
+                Title = "The Alchemist",
+                FirstName = "Paulo",
+                LastName = "Coelho",
+                TotalCopies = 50,
+                CopiesInUse = 35,
+                Type = "Hardcover",
+                ISBN = "1234567895",
+                Category = "Biography"
+            };
+        }
+    }
+}
